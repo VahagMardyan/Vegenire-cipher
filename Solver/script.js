d50de063c65b670d711ecc160bc2567e3035dbac
VahagMardyan <mardyan2003@gmail.com>
2024-03-09 08:41:03 +0400

    Added titles.

--- Solver/script.js
+++ Solver/script.js
@@ -6,5 +6,3 @@ const decryptButton = document.querySelector('.decrypt');
 
 encryptButton.addEventListener('click', encryptMessage);
 decryptButton.addEventListener('click', decryptMessage);
-document.querySelector('.text-message').addEventListener('keypress',encryptMessage);
-document.querySelector('.key-word').addEventListener('keypress',encryptMessage);
